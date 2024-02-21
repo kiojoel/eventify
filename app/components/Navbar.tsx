0caad1dc43e32f6d97bf91c62fb1f66150796191
Akinsanya Joel <akinsanyajoel82@gmail.com>
2024-02-21 18:19:49 +0100

    add user auth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
@@ -26,12 +33,22 @@ export function Navbar() {
           </ul>
         </div>
 
-        <div className="flex items-center gap-x-5 ">
-          <div className="flex items-center gap-x-5">
-            <Button>Sign In</Button>
-            <Button variant="secondary">Sign Up</Button>
+        {(await isAuthenticated()) ? (
+          <LogoutLink>
+            <Button>Log out</Button>
+          </LogoutLink>
+        ) : (
+          <div className="flex items-center gap-x-5 ">
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button variant="secondary">Sign Up</Button>
+              </RegisterLink>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </nav>
   );
